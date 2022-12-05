448a5791041af8dcc7f27f3506cd6aa63cbb91de
Uira.Lima-ext <Uira.Lima-ext@ab-ib-inbev.com>
2022-12-05 19:07:43 -0300

    Começo do projeto, faker

---
+++ aula05-tdd-project-pt01/seed/index.js
@@ -0,0 +1,44 @@
+const faker = require('faker')
+const Car = require('../src/entity/car')
+const CarCategory = require('../src/entity/car-category')
+const Customer = require('../src/entity/customer')
+const { join } = require('path')
+const { writeFile } = require('fs/promises')
+
+const seederBaseFolder = join(__dirname, '../', 'database')
+const ITEMS_AMOUNT = 2
+
+const carCategory = new CarCategory({
+  id: faker.random.uuid(),
+  name: faker.vehicle.type(),
+  carIds: [],
+  price: faker.finance.amount(20, 100)
+})
+
+const cars = []
+const customers = []
+for(let index=0; index <= ITEMS_AMOUNT; index++) {
+  const car = new Car({
+    id: faker.random.uuid(),
+    name: faker.vehicle.model(),
+    available: true,
+    gasAvailable: true,
+    releaseYear: faker.date.past().getFullYear()
+  })
+  const customer = new Customer({
+    id: faker.random.uuid(),
+    name: faker.name.findName(),
+    age: faker.random.number({ min: 18, max: 80 })
+  })
+  customers.push(customer)
+  carCategory.carIds.push(car.id);
+  cars.push(car)
+}
+
+const write = async (filename, data) => writeFile(join(seederBaseFolder, filename), JSON.stringify(data))
+
+;(async () => {
+  await write('cars.json', cars)
+  await write('customer.json', customers)
+  await write('car-category.json', [carCategory])
+})()
