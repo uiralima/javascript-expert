0670fe1a6ee3e1f4213c0e8c119352b48e216664
Uira.Lima-ext <Uira.Lima-ext@ab-ib-inbev.com>
2022-12-05 10:10:18 -0300

    Aula 01

---
+++ aula01-mocks/index.test.js
@@ -0,0 +1,51 @@
+const { error } = require('./src/constants')
+const File = require('./src/file')
+const { rejects, deepStrictEqual } = require('assert')
+
+async function execTest() {
+  {
+    const filePath = './mocks/emptyFile-invalid.csv'
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+    const result = File.csvToJson(filePath)
+    await rejects(result, rejection)
+  }
+  {
+    const filePath = './mocks/fourItems-invalid.csv'
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
+    const result = File.csvToJson(filePath)
+    await rejects(result, rejection)
+  }
+  {
+    const filePath = './mocks/invalid-header.csv'
+    const rejection = new Error(error.FILE_FIELDS_ERROR_MESSAGE)
+    const result = File.csvToJson(filePath)
+    await rejects(result, rejection)
+  }
+  {
+    const expected = [
+      {
+        "name": "Nome1",
+        "id": 123,
+        "profession": "profissão1",
+        "birthDay": new Date().getFullYear() - 1
+      },
+      {
+        "name": "Nome2",
+        "id": 456,
+        "profession": "profissão2",
+        "birthDay": new Date().getFullYear() - 2
+      },
+      {
+        "name": "Nome3",
+        "id": 789,
+        "profession": "profissão3",
+        "birthDay": new Date().getFullYear() - 3
+      }
+    ]
+    const filePath = './mocks/threeItems-valid.csv'
+    const result = await File.csvToJson(filePath)
+    deepStrictEqual(JSON.stringify(result), JSON.stringify(expected))
+  }
+}
+
+execTest()
---
+++ aula01-mocks/src/file.js
@@ -0,0 +1,76 @@
+const { readFile } = require('fs/promises')
+const { error } = require('./constants')
+const User = require('./user')
+
+const DEFAULT_OPTIONS = {
+  maxLines: 3,
+  fields: [ "id", "name", "profession", "age"]
+}
+
+class File {
+  static async csvToJson(filePath) {
+    const content = await File.getFileContent(filePath)
+    const validation = File.isValid(content)
+    if(!validation.valid) {
+      throw new Error(validation.error)
+    }
+    return File.parseCSVToJson(content)
+  }
+
+  static async getFileContent(filePath) {
+    //const fileName = join(__dirname, filePath)
+    return (await readFile(filePath)).toString("utf8")
+  }
+
+  static isValid(csvString, options = DEFAULT_OPTIONS) {
+    const [ header, ...fileWithoutHeader ] = csvString.split('\r\n')
+    const headerArray = header.split(',')
+    const isHeaderValid = (headerArray.join(',') === options.fields.join(','))
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+
+    const isContentLengthAccepted = (
+      fileWithoutHeader.length > 0 &&
+      fileWithoutHeader.length <= options.maxLines
+    )
+
+    if (!isContentLengthAccepted) {
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+
+    return {
+      valid: true
+    }
+  }
+
+  static async parseCSVToJson(csvString) {
+    const lines = csvString.split('\r\n')
+    const firstLine = lines.shift()
+    const headers = firstLine.split(',')
+    const users = lines.map(current => {
+      const properties = current.split(',')
+      const user = {}
+      for(const index in properties) {
+        user[headers[index]] = properties[index]
+      }
+      return new User(user)
+    })
+    return users
+  }
+}
+
+module.exports = File
+
+// (async () => {
+//   //const result = await File.csvToJson('./../mocks/threeItems-valid.csv')
+//   //const result = await File.csvToJson('./../mocks/invalid-header.csv')
+//   const result = await File.csvToJson('./../mocks/fourItems-invalid.csv')
+//   console.log('result', result)
+// })();
