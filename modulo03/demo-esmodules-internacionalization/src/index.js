9b36df1cd8b660476d17b4f723e4b3b5efc3a458
Uira.Lima-ext <Uira.Lima-ext@ab-ib-inbev.com>
2022-12-06 15:21:32 -0300

    Modulo 03 - Demo parte 01 - Intl, Libs(chalk, chalk-table e draftlog)

---
+++ modulo03/demo-esmodules-internacionalization/src/index.js
@@ -0,0 +1,35 @@
+import DraftLog from 'draftlog'
+import chalk from 'chalk'
+import chalkTable from 'chalk-table'
+import readLine from 'readline'
+
+import database from './../database.json'
+import Person from './person.js'
+
+const DEFAULT_LANGUAGE = 'pt-BR'
+
+DraftLog(console).addLineListener(process.stdin)
+
+const options = {
+  leftPad: 2,
+  columns: [
+    { field: "id", name: chalk.cyan("ID") },
+    { field: "vehicles", name: chalk.magenta("Vehicles") },
+    { field: "kmTraveled", name: chalk.cyan("Km Traveled") },
+    { field: "from", name: chalk.cyan("From") },
+    { field: "to", name: chalk.cyan("To") },
+  ]
+}
+
+const table = chalkTable(options, database.map(m => new Person(m).formatted(DEFAULT_LANGUAGE)))
+const print = console.draft(table) 
+
+const terminal = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+terminal.question('Qual é o seu nome?', msg => {
+  console.log('msg', msg.toString())
+})
+
